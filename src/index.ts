2a353474ebd8fae158434d40fc679e66857dc349
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2021-11-10 19:31:15 +0700

    configure eslint + prettier

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import * as express from 'express';
-import { Request, Response } from 'express';
+import * as express from "express";
+import { Request, Response } from "express";
+
 const app = express();
 
 const { PORT = 3000 } = process.env;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send({
-    message: 'hello world',
-  });
+app.get("/", (req: Request, res: Response) => {
+  res.send({ message: "hello world" });
 });
 
-if (require.main === module) { // true if file is executed
+if (require.main === module) {
+  // true if file is executed
   app.listen(PORT, () => {
-    console.log('server started at http://localhost:'+PORT);
+    console.log("server started at http://localhost:" + PORT);
   });
 }
 
