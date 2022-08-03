7cb69462678212004851736cd0fddce79aabe040
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2022-08-03 23:54:39 +0700

    refactor arch pattern

--- src/core/entity/user.entity.ts
+++ src/core/entity/user.entity.ts
@@ -1,45 +1,45 @@
 export enum Gender {
-  MALE = 'male',
-  FEMALE = 'female',
+  MALE = "male",
+  FEMALE = "female",
 }
 
 export enum MaritalStatus {
-  MARRIED = 'married',
-  SINGLE = 'single',
-  WIDOW = 'widow',
-  WIDOWER = 'widower',
+  MARRIED = "married",
+  SINGLE = "single",
+  WIDOW = "widow",
+  WIDOWER = "widower",
 }
 
 export enum Religion {
-  ISLAM = 'islam',
-  CHRISTIANITY = 'christianity',
-  PROTESTANTISM = 'protestantism',
-  HINDUISM = 'hinduism',
-  BUDDHISM = 'buddhism',
-  CONFUCIANISM = 'confucianism',
-  OTHER = 'other',
+  ISLAM = "islam",
+  CHRISTIANITY = "christianity",
+  PROTESTANTISM = "protestantism",
+  HINDUISM = "hinduism",
+  BUDDHISM = "buddhism",
+  CONFUCIANISM = "confucianism",
+  OTHER = "other",
 }
 
 export enum IdentityType {
-  KTP = 'KTP',
-  SIM = 'SIM',
-  KK = 'KK',
-  OTHER = 'other',
+  KTP = "KTP",
+  SIM = "SIM",
+  KK = "KK",
+  OTHER = "other",
 }
 
 export enum BloodType {
-  A = 'A',
-  A_POSITIVE = 'A+',
-  A_NEGATIVE = 'A-',
-  B = 'B',
-  B_POSITIVE = 'B+',
-  B_NEGATIVE = 'B-',
-  O = 'O',
-  O_POSITIVE = 'O+',
-  O_NEGATIVE = 'O-',
-  AB = 'AB',
-  AB_PABSITIVE = 'AB+',
-  AB_NEGATIVE = 'AB-',
+  A = "A",
+  A_POSITIVE = "A+",
+  A_NEGATIVE = "A-",
+  B = "B",
+  B_POSITIVE = "B+",
+  B_NEGATIVE = "B-",
+  O = "O",
+  O_POSITIVE = "O+",
+  O_NEGATIVE = "O-",
+  AB = "AB",
+  AB_PABSITIVE = "AB+",
+  AB_NEGATIVE = "AB-",
 }
 
 export interface UserEntityInterface {
